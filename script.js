30ed4fb66bed903b9d65dd2704e005bbcfa61eed
ztheilen <ztheilen@coppertreeanalytics.com>
2022-03-31 11:20:52 -0700

    learning about variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello from a serperated file!');
+
+var myName = 'Zachary';
+console.log(myName);
+
+var schoolTeam = 'mixed martial arts';
+var schoolNumber = 42;
+
+var isRegistered = true;
+console.log(isRegistered);
