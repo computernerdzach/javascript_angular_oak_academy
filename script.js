08dd3b883c236bf206f3d4caa0ffedfbc48136ca
ztheilen <ztheilen@coppertreeanalytics.com>
2022-03-31 11:37:12 -0700

    type coercion

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ console.log(myName);
 
 var schoolTeam = 'mixed martial arts';
 var schoolNumber = 42;
+console.log(schoolNumber)
 
 var isRegistered = true;
 console.log(isRegistered);
@@ -13,4 +14,13 @@ var job;
 console.log(job);
 
 job = 'student';
-console.log(job)
+console.log(job);
+
+console.log(myName + ' ' + schoolNumber);
+
+var schoolName, hasCourse;
+
+schoolName = 'OAKAcademy';
+hasCourse = true;
+
+console.log(schoolName + ' has 35 courses. Does it have any course? ' + hasCourse);
