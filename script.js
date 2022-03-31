39ffa7570c52d53ee9f7d23870e341a881cc5042
ztheilen <ztheilen@coppertreeanalytics.com>
2022-03-31 11:51:09 -0700

    mutate variables, dynamic typing, alert and prompt

--- script.js
+++ script.js
@@ -23,4 +23,17 @@ var schoolName, hasCourse;
 schoolName = 'OAKAcademy';
 hasCourse = true;
 
-console.log(schoolName + ' has 35 courses. Does it have any course? ' + hasCourse);
+console.log(
+    schoolName + ' has 35 courses. Does it have any course? ' + hasCourse
+);
+
+schoolNumber = 'forty two';
+console.log(schoolNumber)
+
+schoolTeam = 'tennis';
+console.log(schoolTeam);
+
+alert(schoolTeam);
+
+var age = prompt('How old is he?');
+console.log('Alex is ' + age + ' years old.');
