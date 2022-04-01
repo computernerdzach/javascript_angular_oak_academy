22c098af3075bbcc0250b9d8c77156c3a41f60fc
ztheilen <ztheilen@coppertreeanalytics.com>
2022-04-01 10:48:14 -0700

    basic and logical operators

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
-console.log('Hello from a serperated file!');
+// console.log('Hello from a serperated file!');
 
-var myName = 'Zachary';
-console.log(myName);
+// var myName = 'Zachary';
+// console.log(myName);
 
-var schoolTeam = 'mixed martial arts';
-var schoolNumber = 42;
-console.log(schoolNumber)
+// var schoolTeam = 'mixed martial arts';
+// var schoolNumber = 42;
+// console.log(schoolNumber)
 
-var isRegistered = true;
-console.log(isRegistered);
+// var isRegistered = true;
+// console.log(isRegistered);
 
-var job;
-console.log(job);
+// var job;
+// console.log(job);
 
-job = 'student';
-console.log(job);
+// job = 'student';
+// console.log(job);
 
-console.log(myName + ' ' + schoolNumber);
+// console.log(myName + ' ' + schoolNumber);
 
-var schoolName, hasCourse;
+// var schoolName, hasCourse;
 
-schoolName = 'OAKAcademy';
-hasCourse = true;
+// schoolName = 'OAKAcademy';
+// hasCourse = true;
 
-console.log(
-    schoolName + ' has 35 courses. Does it have any course? ' + hasCourse
-);
+// console.log(
+//     schoolName + ' has 35 courses. Does it have any course? ' + hasCourse
+// );
 
-schoolNumber = 'forty two';
-console.log(schoolNumber)
+// schoolNumber = 'forty two';
+// console.log(schoolNumber)
 
-schoolTeam = 'tennis';
-console.log(schoolTeam);
+// schoolTeam = 'tennis';
+// console.log(schoolTeam);
 
-alert(schoolTeam);
+// alert(schoolTeam);
 
-var age = prompt('How old is he?');
-console.log('Alex is ' + age + ' years old.');
+// var age = prompt('How old is he?');
+// console.log('Alex is ' + age + ' years old.');
+
+
+
+//////////// BASIC OPERATORS ////////////
+var totalDistance, distanceAlex, distanceSam;
+var totalDistance = 20;
+
+var distanceAlex = totalDistance - 12;
+console.log(distanceAlex);
+var distanceSam = totalDistance - 7;
+console.log(distanceSam);
+console.log(distanceSam * 2);
+console.log(distanceSam / 2);
+
+// logical operators //
+var distanceToSchool, distanceToHome;
+
+distanceToHome = 14;
+distanceToSchool = 6;
+
+var isCloser = distanceToHome > distanceToSchool;
+console.log(isCloser);
